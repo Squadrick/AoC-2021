68f0833e00d79b871b99600861958ee70a9f248e
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2021-12-03 11:47:08 +0530

    Day 1

---
+++ 1/1.rs
@@ -0,0 +1,17 @@
+use std::io::BufRead;
+
+fn main() {
+  let stdin = std::io::stdin();
+  let mut lines = stdin.lock().lines();
+  let mut count: i32 = 0;
+  let mut prev: i64 = lines.next().unwrap().unwrap().trim().parse().unwrap();
+
+  while let Some(line) = lines.next() {
+    let value: i64 = line.unwrap().trim().parse().unwrap();
+    if value > prev {
+      count += 1;
+    }
+    prev = value;
+  }
+  println!("Count: {}", count);
+}
